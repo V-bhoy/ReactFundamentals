ebcbebee4da0c9b2a83feb0fd870b0d3b192fae8
VBhoy <83490034+V-bhoy@users.noreply.github.com>
2025-04-26 16:42:32 +0530

    add plugins & config

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Title from "./Title";
 
+// core react element
 const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!")
 // console.log(heading); // this is a React element which is a plain js object that describes the DOM node
 // this will not print the element but an object
 
 
-// lets render a nested html elements
+// lets render a nested react elements
 const parent = React.createElement("div", {id: "parent"},
                         React.createElement("div", {id: "child"},
                              [
@@ -19,10 +21,15 @@ const parent = React.createElement("div", {id: "parent"},
 // It becomes more complicated, difficult to understand and maintain
 // What is the solution ? JSX
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 // console.log(parent); // this is also a plain javascript object which has nested objects within it.
 // if there are any child elements into our root node, it will get replaced by the React element
 // that is passed in the render method
 // Any html elements above or below the root element stay as it is in the body.
 // This means react only works in the target root element, it can be anything ,div, footer, header anything.
+
+// react element using JSX
+// importing Title from .jsx file because vite does not allow to use jsx in js file
+// console.log(Title()); //--> this log the React element only that is the JS object. It returns a pure React element
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(Title());
